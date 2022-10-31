85bf6f481b912f9feeaddaf627329c7ea8873ea0
mlabar26 <mlabar26@byu.edu>
2022-10-30 20:09:16 -0600

    marcus' update

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@
 from django.http import HttpResponse
 
 def indexPageView(request) :
-    sOutput = '<html>'\
+    sOutput='<html>'\
                 '<head><title>Home</title></head>'\
                 '<body>'\
                     '<p style="font-size:50px; background-color:#779ecb; text-align:center; color:white;">Welcome to QRMyAncestors (pun intended)!</p>'\
@@ -19,9 +19,26 @@ def indexPageView(request) :
     return HttpResponse(sOutput)   
 
 def aboutPageView(request) :
-    sOutput = '<html><head><title>About</title></head><body><p style="font-size:50px; color:white; background-color:#779ecb; text-align:center;">About QRMyAncestors:</p><p style="font-size:30px; color:white; background-color:#779ecb; text-align:center;">Started in 2022, QRMyAncestors is beginning to revolutionize the way we share stories and remember those who have passed away. Stay connected with us :)</p></body><ul style="font-size:20px; color:white; background-color:#779ecb;"><li>email: QRMyAncestors@QRMyAncestors.com</li><li>instagram: QRMyAncestors</li><li>LinkedIn: QRMyAncestors</li></ul></body></html>'
+    sOutput='<html>'\
+                '<head><title>About</title></head>'\
+                '<body>'\
+                    '<p style="font-size:50px; color:white; background-color:#779ecb; text-align:center;">About QRMyAncestors:</p>'\
+                    '<p style="font-size:30px; color:white; background-color:#779ecb; text-align:center;">Started in 2022, QRMyAncestors is beginning to revolutionize the way we share stories and remember those who have passed away. Stay connected with us :)</p>'\
+                '</body>'\
+                '<ul style="font-size:20px; color:white; background-color:#779ecb;">'\
+                    '<li>Email: QRMyAncestors@QRMyAncestors.com</li>'\
+                    '<li>Instagram: QRMyAncestors</li>'\
+                    '<li>LinkedIn: QRMyAncestors</li>'\
+                '</ul>'\
+                '</body>'\
+            '</html>'
     return HttpResponse(sOutput)
 
 def profilePageView(request, person_name) :
-    sOutput = '<html><head><title>Contact</title></head><body><p style="font-size:50px; color:white; background-color:#779ecb; text-align:center;">About ' + person_name + '\'s story</p></body></html>'
+    sOutput='<html>'\
+                '<head><title>Contact</title></head>'\
+                '<body>'\
+                    '<p style="font-size:50px; color:white; background-color:#779ecb; text-align:center;">About ' + person_name + '\'s Story</p>'\
+                '</body>'\
+            '</html>'
     return HttpResponse(sOutput)
