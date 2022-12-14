8771c36c2bc205935a113f82339980b1a61aead6
wirickad <92128478+wirickad@users.noreply.github.com>
2022-12-13 22:17:05 -0700

    to deploy

--- qrmyancestors/settings.py
+++ qrmyancestors/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
+
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 from getpass import getpass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j-yisgmcm%9+)z_^t++a-+@0s_odvxwwe46wk@@abm1-^e^g*b'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
