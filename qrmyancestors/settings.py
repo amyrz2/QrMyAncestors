c505621b508a71dac886728445052a9de7104e7d
wirickad <92128478+wirickad@users.noreply.github.com>
2022-12-13 22:35:53 -0700

    Update settings.py

--- qrmyancestors/settings.py
+++ qrmyancestors/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'qrmyancestors.urls'
